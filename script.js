10f4bd197a2da67363f4db900f174e9843f2c22a
saratbhaskaran <sarathkaruvanthala@gmail.com>
2021-10-06 18:52:34 +0530

    my new changes

--- script.js
+++ script.js
@@ -1,34 +1,26 @@
 let nameOfStdnt = document.getElementById("nameip")
 let ageOfStdnt = document.getElementById('ageip')
 let gradeOfStdnt = document.getElementById('grdip')
-// let addDetails=document.getElementById('add')
 let tdOne = document.getElementById('tdone')
 let tdTwo = document.getElementById('tdtwo')
 let tdThree = document.getElementById('tdthree')
-let count=0;
+let count = 0;
+let coudnt = 0;
 
 
-function dlt(id){
-        console.log('id',id)
-        // let trow= document.getElementsByClassName(`s${id}`)
-        let trow = document.getElementById(`s${id}`)
-
-        console.log(trow)
-        trow.remove();
-    }
-   
-
-
-
+function dlt(id) {
+    console.log('id', id)
+    let trow = document.getElementById(`s${id}`)
+    console.log(trow)
+    trow.remove();
+}
 
 
 let addDetails = function () {
-    let trow= document.getElementsByClassName(`s1`)
-    console.log(trow)
+    let trow = document.getElementsByClassName(`s1`)
     let stdntnm = nameOfStdnt.value;
     let stdntage = ageOfStdnt.value;
     let stdntgrd = gradeOfStdnt.value;
-
 
 
     let pushObject = {
@@ -36,26 +28,22 @@ let addDetails = function () {
         agee: stdntage,
         grdee: stdntgrd
     }
-    
+
 
     if (stdntnm && stdntage && stdntgrd) {
-        
+
         let table = document.getElementById('myTable');
-        count+=1;
+        count += 1;
         ;
         let row = `<tr id="s${count}">
             <td>${pushObject.namee}</td>
             <td>${pushObject.agee}</td>
             <td>${pushObject.grdee}</td>
             <td><button id="${count}" class="buttn" onclick="dlt(this.id)">remove</button></td>
-            </tr>`  
+            </tr>`
         table.innerHTML += row;
-        
-     
-   
-
-       
     }
+
     else {
         alert('please fill compelete details')
     }
@@ -63,8 +51,6 @@ let addDetails = function () {
     nameOfStdnt.value = "";
     ageOfStdnt.value = "";
     gradeOfStdnt.value = "";
-
-
 
 }
 
